80ee0dcec03b1e4ce5f127be73f05b269a1231e1
juliet@21 <julietyartes2@gmail.com>
2024-10-16 12:36:45 -0500

    archivo de taller de js

---
+++ script.js
@@ -0,0 +1,59 @@
+function generarHistoria (nombre, lugar, objeto) {
+    
+    let historia = nombre + " fue a " + lugar + " y encontró unos " + objeto + ".";
+    return historia;
+}
+
+let nombre = "maria";
+let lugar = "bogota";
+let objeto = "dados";
+
+historia = generarHistoria(nombre, lugar, objeto);
+
+console.log(historia);
+
+
+
+function calcularDescuento ( precio , porcentajeDescuento ) {
+    
+    }
+    // Ejemplo de uso
+    const precioFinal = calcularDescuento (100 , 20) ;
+    console.log ("El precio con descuento es : $${ precioFinal } ") ;
+
+
+
+
+function celsiusAFahrenheit ( celsius ) {
+    let celsiusAFahrenheit =celsius;
+    return celsiusAFahrenheit;
+    }
+    function fahrenheitACelsius ( fahrenheit ) {
+    let fahrenheitACelsius=fahrenheit;
+    }
+    
+    console . log ( celsiusAFahrenheit (25) ) ;
+    console . log ( fahrenheitACelsius (77) ) ;
+
+
+    function generarContrasena ( longitud , incluirNumeros ,
+        incluirSimbolos ) {
+         let generarContrasena =longitud +"contrsaseña" +incluirNumeros+"numeros"+incluirSimbolos+"simbolos";
+        }
+        const nuevaContrasena = generarContrasena (12 , true , true );
+        console . log (" Tu nueva contrasena es : ${ .......} ") ;
+
+
+
+
+        function viajarEnElTiempo ( fechaNacimiento , edad ) {
+            let viajarEnElTiempo ="fecha:"+ fechaNacimiento + "edad:"+ edad;
+            return
+            }
+        
+            const anoFuturo = viajarEnElTiempo ("1976 -05 -23" , 50) ;
+            console.log(" Tendras 50 años en el año 2024 " ) ;
+
+
+            
+            
